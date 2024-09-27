5d5906804790306ccea8d99681d9593414b04464
Tomas Angel <t.angel@uniandes.edu.co>
2024-09-26 23:34:35 -0500

    Inicio de la implementacion del RatingView

---
+++ UniPark/Core/Authentication/View/ReviewView.swift
@@ -0,0 +1,24 @@
+//
+//  ReviewView.swift
+//  UniPark
+//
+//  Created by Tomas Angel on 26/09/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct ReviewView: View {
+    @State var parkingSpot: ParkingDetail
+    @State var review: Review
+    
+    var body: some View {
+        Text("Hello world")
+    }
+}
+
+struct ReviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReviewView()
+    }
+}
